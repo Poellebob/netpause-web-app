641c202607efddaadad114a24a4fa151411508ce
ThorDL <166723685+ThorDL@users.noreply.github.com>
2024-11-05 13:01:37 +0100

    Jeg kan ikke rigtig huske hvad jeg har lavet denne gang

--- js/schedule.js
+++ js/schedule.js
@@ -3,39 +3,35 @@ console.log('Started');
 const knap1 = document.getElementById('Knap1');
 const knap2 = document.getElementById('Knap2');
 const outputElement = document.getElementById('output');
-
+let userInputHandling 
+let userInputDato 
+let userInputTidsrum
+let lowerCaseInput 
 
 knap1.addEventListener('click',knap1Click);
-knap2.addEventListener('click',knap2Click);
 
-
-
-function knap1Click () {
+function knap1Click () {  
+    userInputHandling = document.getElementById('userInputHandling').value;
+    userInputDato = document.getElementById('userInputDato').value;
+    userInputTidsrum = document.getElementById('userInputTidsrum').value;
+    lowerCaseInput = userInputDato.toLowerCase();
     console.log('knap1virker');
-    const userInputHandling = document.getElementById('userInputHandling').value;
-    const userInputDato = document.getElementById('userInputDato').value;
-    const userInputTidsrum = document.getElementById('userInputTidsrum').value;
-    const lowerCaseInput = userInputDato.toLowerCase();
     console.log(userInputHandling);
     console.log(userInputDato); 
     console.log(userInputTidsrum);
-    outputElement.innerHTML = `Du vil ${userInputHandling} på ${userInputDato} kl. ${userInputTidsrum}`;
-    function isValidDay(lowerCaseInput) {
-        const daysOfWeek = ["søndag", "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag"];
-        return daysOfWeek.includes(lowerCaseInput);
-      }
+    if (isValidDay(lowerCaseInput)) {
+      console.log("Valid day!");
+      outputElement.innerHTML = `Du vil ${userInputHandling} på ${userInputDato} kl. ${userInputTidsrum}`;
+    } else {
+      console.log("Invalid day!");
+      outputElement.innerHTML = 'Angiv en gyldig ugedag';
+    }
+}
+
     
-      if (isValidDay(lowerCaseInput)) {
-        console.log("Valid day!");
-      } else {
-        console.log("Invalid day!");
-      }
-
+function isValidDay(Ugedag) {
+  const daysOfWeek = ["søndag", "mandag", "tirsdag", "onsdag", "torsdag", "fredag", "lørdag"];
+  return daysOfWeek.includes(Ugedag);
 }
-
-function knap2Click () {
-    console.log('knap2virker');
-}
-
-
+    
 
